a4d080e947e00cd6eb49fd8fe8ba932556980fcd
lpatmo <linda.peng@alumni.duke.edu>
2015-05-02 16:38:45 -0400

    added iron router and an about page

--- ab.js
+++ ab.js
@@ -2,13 +2,13 @@ if (Meteor.isClient) {
   // counter starts at 0
   Session.setDefault('counter', 0);
 
-  Template.goals.helpers({
+  Template.goalsList.helpers({
     counter: function () {
       return Session.get('counter');
     }
   });
 
-  Template.goals.events({
+  Template.goalsList.events({
     'click button': function () {
       // increment the counter when button is clicked
       Session.set('counter', Session.get('counter') + 1);
